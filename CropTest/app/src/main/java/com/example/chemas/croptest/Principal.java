a67a5d057951a0303e48f92493be46068c727a86
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-01-24 16:41:53 -0600

    Initial Commit

---
+++ CropTest/app/src/main/java/com/example/chemas/croptest/Principal.java
@@ -0,0 +1,80 @@
+package com.example.chemas.croptest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+public class Principal extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button normal;
+    Button test;
+    ImageView imagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+        normal = findViewById(R.id.normal);
+        test = findViewById(R.id.test);
+        imagen = findViewById(R.id.img);
+
+        normal.setOnClickListener(this);
+        test.setOnClickListener(this);
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                Picasso.with(this).load(resultUri).into(imagen);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.normal:
+                cropImage();
+                break;
+        }
+
+    }
+
+
+    public void cropImage(){
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.OFF)
+                .setFixAspectRatio(true)
+                .setAspectRatio(19,6)
+                .setActivityTitle("Recortando")
+                
+                .start(this);
+
+    }
+}
